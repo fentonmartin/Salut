90c9488723514445ce4052d78930fdd5648efd1b
Fenton Martin <fentonxm@gmail.com>
2018-12-04 22:54:03 +0700

    Remove unused import on Main Activity

--- demo/src/main/java/com/peak/salut/demo/MainActivity.kt
+++ demo/src/main/java/com/peak/salut/demo/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.peak.salut.demo
 
-import android.app.PendingIntent.getActivity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
